91540d626a27b7e12a1ed047f45082884e7e4570
Elena Komendantova <lenacom@rambler.ru>
2026-07-12 17:08:41 +0300

    button + dialog

--- src/verbs/fr/verbs_code.js
+++ src/verbs/fr/verbs_code.js
@@ -85,3 +85,95 @@ function findVerbTenses(form) {
     }
   }
 }
+
+async function getYandexTranslation(text) {
+	const url = new URL("https://dictionary.yandex.net/dicservice.json/lookupMultiple");
+	url.searchParams.set("ui", "ru");
+	url.searchParams.set("lang", "fr-ru");
+	url.searchParams.set("dict", "fr-ru.regular");
+	url.searchParams.set("type", "regular");
+	url.searchParams.set("flags", "15783");
+	url.searchParams.set("srv", "tr-text");
+	url.searchParams.set("text", text);
+	const response = await fetch(url);
+	const json = await response.json();
+
+	const regular = json["fr-ru"]["regular"];
+	if (!regular || regular.length === 0) {
+		return undefined;
+	}
+
+	return regular.map(item => {
+		const word = item.text;
+		let ts = item.ts ?? "";
+		if (ts) {
+			ts = `[${ts}]`;
+		}
+		const tr = item.tr[0].text;
+		const gen = item.gen?.code ?? "";
+		return [word, ts, gen, tr].filter(it => it).join(" ");
+	}).map(it => `<div>${it}</div>`);
+}
+
+async function translate(text) {
+  const button = document.getElementById('fr-helper-btn');
+
+  if (!text) {
+    button.style.display = 'none';
+    return;
+  }
+
+  const translation = await getYandexTranslation(text);
+
+  const verbTenses = findVerbTenses(text);
+  let conjugation;
+  if (verbTenses !== undefined) {
+    const tenseIds = formToTenses[text];
+    conjugation = `${verbTenses.infinitives}${verbTenses.type === "irregular" ? "*" : ""}: ${verbTenses.tenses}`;
+  }
+
+  if (!translation && ! conjugation) {
+    button.style.display = 'none';
+    return;
+  }
+
+  let content = `<button onClick="this.parentElement.close();">x</button><h2>${text}</h2>`;
+
+  if (translation) {
+    content += `<div>${translation.join(" ")}</div>`;
+  }
+  if (conjugation) {
+    content += `<div>${conjugation}</div>`;
+  }
+  content += `<input onClick='translation("${text}", "fr", "ru")' type="button" value="Google Translate" class="secondary rounded"/>`;
+
+  const helper = document.getElementById('fr-helper');
+  helper.innerHTML = `<button id="fr-helper-btn" style="position: fixed; top:0; left:0; z-index: 100" onClick="this.style.display = 'none'; document.getSelection().removeAllRanges(); document.getElementById('fr-helper-dialog').showModal();">
+    ${text.substring(0, 20) + (text.length > 20? "&hellip;" : "")}
+    </button>
+    <dialog id="fr-helper-dialog">
+    ${content}
+    </dialog>`;
+}
+
+document.addEventListener("selectionchange", async () => {
+  await translate(document.getSelection().toString());
+});
+
+function openURL(url) {
+	const width = Math.min(window.innerWidth, 1024);
+	const height = Math.min(window.innerHeight, 768);
+	window.open(url, '_blank', `width=${width}, height=${height}`);
+}
+
+function translation(text, fromLang, toLang) {
+	const src = new URL("https://translate.google.com/details");
+	src.searchParams.set("hl", "ru");
+	src.searchParams.set("tl", toLang);
+	src.searchParams.set("sl", fromLang);
+	src.searchParams.set("text", text);
+	src.searchParams.set("op", "translate");
+	openURL(src.toString());
+}
+
+// TODO https://www.le-francais.ru/conjugaison/rappeler/
