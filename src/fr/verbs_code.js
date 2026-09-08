95b0f34807661f1cf9804cba3f43baff42c47aac
ONTARGET\ekomendantova <ekomendantova@infobase.com>
2026-09-08 20:34:33 +0200

    removed close button to dialog Learn

--- src/fr/verbs_code.js
+++ src/fr/verbs_code.js
@@ -257,8 +257,7 @@ function learnDialogHTML(id, item) {
     })
     .join("");
 
-  return `<dialog id="${id}" style="position: relative;">
-      <button style="position: absolute; top: 0.25rem; right: 0.25rem; margin: 0; padding: 0.25rem 0.5rem; border: none; background: transparent; cursor: pointer;" onClick="document.getElementById('${id}').close();">✕</button>
+  return `<dialog id="${id}">
       <div id="${id}_part1">${part1}</div>
       <div style="display: flex; flex-direction: row; flex-wrap: wrap; gap: 0.5rem;">${buttons}</div>
     </dialog>`;
